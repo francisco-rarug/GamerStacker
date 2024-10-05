23ad533b42bd7f0301a81719d1cff46372017901
francisco-rarug <rarugfrancisco@gmail.com>
2024-10-04 21:43:49 -0300

    WIP manejo de errores

--- Scripts/carrito.js
+++ Scripts/carrito.js
@@ -2,81 +2,80 @@ document.querySelectorAll('.btn-agregar-carrito').forEach((boton, index) => {
     boton.addEventListener('click', () => {
         const nombreProducto = document.querySelectorAll('.data-producto')[index].innerText;
         const precioProducto = document.querySelectorAll('.data-precio')[index].innerText;
-        const imgProducto = document.querySelectorAll('.image-box img')[index].src;
+        const imgProducto = document.querySelectorAll('.image-box img')[index].src
         const boton = document.querySelectorAll('.btn-agregar-carrito')[index]
         
-        boton.style.display = "none"
-        const producto = { nombre: nombreProducto, precio: precioProducto, img: imgProducto};
+        const producto = { nombre: nombreProducto, precio: precioProducto, img: imgProducto}
+        
+        let productosCarrito = JSON.parse(localStorage.getItem('carrito')) || []
+        
+        productosCarrito.push(producto)
 
-        let productosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+        localStorage.setItem('carrito', JSON.stringify(productosCarrito))
 
-        productosCarrito.push(producto);
-
-        localStorage.setItem('carrito', JSON.stringify(productosCarrito));
-
-        const modal = document.getElementById("modal");
-        modal.style.display = "flex";
+        const modal = document.getElementById("modal")
+        modal.style.display = "flex"
         
 
-        modalInteracciones();
-    });
-});
+        modalInteracciones()
+    })
+})
 
 function cargarCarrito() {
-    let productosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    let productosCarrito = JSON.parse(localStorage.getItem('carrito')) || []
 
-    const listaProductos = document.getElementById('productos-lista');
+    const listaProductos = document.getElementById('productos-lista')
 
-    listaProductos.innerHTML = '';
+    listaProductos.innerHTML = ''
 
     if (productosCarrito.length === 0) {
-        listaProductos.innerHTML = '<li>El carrito está vacio</li>';
+        listaProductos.innerHTML = '<li>El carrito está vacio</li>'
     } else {
         productosCarrito.forEach(producto => {
-            const li = document.createElement('li');
+            const li = document.createElement('li')
             li.innerHTML = `
                 <img src="${producto.img}" alt="${producto.nombre}" width="50" height="50"> 
-                - <strong>${producto.nombre}</strong> - ${producto.precio}`;
-            listaProductos.appendChild(li);
-        });
+                - <strong>${producto.nombre}</strong> - ${producto.precio}`
+            listaProductos.appendChild(li)
+        })
 
     }
 }
 
-cargarCarrito();
+cargarCarrito()
 
 
 function modalInteracciones() {
-    const modal = document.getElementById("modal");
-    const botonFinalizarCompra = document.getElementById("finalizar-compra");
-    const botonSeguirComprando = document.getElementById("seguir-comprando");
-    const close = document.querySelector(".close");
+    const modal = document.getElementById("modal")
+    const botonFinalizarCompra = document.getElementById("finalizar-compra")
+    const botonSeguirComprando = document.getElementById("seguir-comprando")
+    const close = document.querySelector(".close")
 
     close.addEventListener("click", () => {
-        modal.style.display = "none";
-    });
+        modal.style.display = "none"
+    })
 
     botonFinalizarCompra.addEventListener("click", () => {
-        window.location.href = "/Secciones/carrito.html";
-    });
+        window.location.href = "/Secciones/carrito.html"
+    })
 
     botonSeguirComprando.addEventListener("click", () => {
-        modal.style.display = "none";
-    });
+        modal.style.display = "none"
+    })
 
     window.addEventListener("click", (event) => {
         if (event.target == modal) {
-            modal.style.display = "none";
+            modal.style.display = "none"
         }
-    });
+    })
 }
-const borrarBtn = document.getElementById("borrar");
+const borrarBtn = document.getElementById("borrar")
 if (borrarBtn) {
-    borrarBtn.onclick = borrar;
+    borrarBtn.onclick = borrar
     boton.style.display = "flex"
 }
 function borrar() {
-    localStorage.removeItem('carrito');
-    cargarCarrito();
+    localStorage.removeItem('carrito')
+    cargarCarrito()
 }
 
